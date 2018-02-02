43b326b3e9da72a161fdb82b0e6272b9157642d4
Steve Melia <steve.j.melia@gmail.com>
2018-02-02 14:13:00 +0000

    Add sample of named services in constructor

---
+++ README.md
@@ -0,0 +1,6 @@
+Castle Windsor Named Services In Constructor Injection
+======================================================
+
+Sample of using named services in constructor injection using Windsor.
+
+Won't work with .NET fiddle as it's a medium trust environment.
--- WindsorTest/Program.cs
+++ WindsorTest/Program.cs
@@ -1,15 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WindsorTest
+﻿namespace WindsorTest
 {
+    using Castle.MicroKernel.Registration;
+    using Castle.Windsor;
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var container = new WindsorContainer();
+
+            container.Register(Component.For<IMyDependency>().ImplementedBy<MyDependencyA>().Named("a"));
+            container.Register(Component.For<IMyDependency>().ImplementedBy<MyDependencyB>().Named("b"));
+            container.Register(Component.For<Consumer>()
+                .DependsOn(ServiceOverride.ForKey("a").Eq("a"))
+                .DependsOn(ServiceOverride.ForKey("b").Eq("b")));
+
+            Console.WriteLine(container.Resolve<IMyDependency>("a").GetString);
+            Console.WriteLine(container.Resolve<IMyDependency>("b").GetString);
+            container.Resolve<Consumer>().Write();
+            Console.ReadKey();
+        }
+    }
+
+    interface IMyDependency
+    {
+        string GetString { get; }
+    }
+
+    class MyDependencyA : IMyDependency
+    {
+        public string GetString {  get { return "A"; } }
+    }
+
+    class MyDependencyB : IMyDependency
+    {
+        public string GetString { get { return "B"; } }
+    }
+
+    class Consumer
+    {
+        private IMyDependency a;
+
+        private IMyDependency b;
+
+        public Consumer(IMyDependency a, IMyDependency b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void Write()
+        { 
+            Console.WriteLine(a.GetString);
+            Console.WriteLine(b.GetString);
         }
     }
 }
